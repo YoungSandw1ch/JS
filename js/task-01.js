a475ca34472110a233112f1e0b1fb5b1625dc310
Sergei Mikhailenko <sersus1991@gmail.com>
2022-07-28 00:45:58 +0300

    add function

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,27 @@
 const categoriesItemsRefs = document.querySelectorAll('#categories .item');
 
+//-----------обычное логирование-----------------------
+// console.log('Number of categories: ', categoriesItemsRefs.length);
+
+// categoriesItemsRefs.forEach(item => console.log(
+//   `Category: ${item.querySelector('h2').textContent}
+// Elements: ${item.querySelectorAll('li').length}`));
+
+//------------количество в цифрах а не строках----------------------
 console.log('Number of categories: ', categoriesItemsRefs.length);
 
-categoriesItemsRefs.forEach(item => console.log(
-  `Category: ${item.querySelector('h2').textContent}
-Elements: ${item.querySelectorAll('li').length}`));
+categoriesItemsRefs.forEach(item => {
+  console.log(`Category: ${item.querySelector('h2').textContent}`);
+  console.log('Elements: ', item.querySelectorAll('li').length);
+});
 
-// categoriesItemsRefs.forEach(item => {
-//   console.log(`Category: ${item.querySelector('h2').textContent}`);
-//   console.log('Elements: ', item.querySelectorAll('li').length);
-// });
+//------------функция----------------------------------
+// const categoriesLogger = categories => {
+//   console.log('Number of categories: ', categoriesItemsRefs.length);
 
+//   categories.forEach(categorie => console.log(
+//     `Category: ${categorie.querySelector('h2').textContent} 
+// Elements: ${categorie.querySelectorAll('li').length}`))
+// };
 
+// categoriesLogger(categoriesItemsRefs);
