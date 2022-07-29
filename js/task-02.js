5786bdddcdb7335e982ec33fc9a1b3670a01e739
Sergei Mikhailenko <sersus1991@gmail.com>
2022-07-29 12:43:28 +0300

    6hm 2nd task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const createIngredients = ingredients => {
+  return ingredients.map(el => {
+    const ingredientRef = document.createElement('li');
+    ingredientRef.classList.add('item');
+    ingredientRef.textContent = el;
+    return ingredientRef;
+  })
+}
+
+ingredientsRef.append(...createIngredients(ingredients));
+
