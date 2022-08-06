76bfcbf1e2a5474b262c400fc08b51d7654ce2f8
Sergei Mikhailenko <sersus1991@gmail.com>
2022-08-06 11:39:48 +0300

    6hw 8task done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+  email: document.querySelector('[name="email"]'),
+  password: document.querySelector('[name="password"]'),
+  btn: document.querySelector('[type="submit"]'),
+}
 
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const { elements: { email, password } } = e.currentTarget;
+
+  if (!password.value || !email.value) return alert("Вы должны заполнить все поля");
+  const values = {
+    email: email.value,
+    password: password.value,
+  }
+
+  console.log(values);
+  e.currentTarget.reset();
+}
