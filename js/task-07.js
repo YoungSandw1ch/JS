0e7000811ad6d694d995b386603b4c7bf7c7668a
Sergei Mikhailenko <sersus1991@gmail.com>
2022-08-06 10:59:24 +0300

    6hw 7task done

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+const refs = {
+  input: document.querySelector('#font-size-control'),
+  span: document.querySelector('#text'),
+};
+
+refs.input.addEventListener('input', onRangeChange);
+refs.span.style.fontSize = `${refs.input.value}px`;
+
+function onRangeChange(event) {
+  refs.span.style.fontSize = `${event.currentTarget.value}px`;
+}
