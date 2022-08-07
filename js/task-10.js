26efc7da905b1446932c8907a833de89213e5e7b
Sergei Mikhailenko <sersus1991@gmail.com>
2022-08-07 10:26:40 +0300

    10task fix

--- js/task-10.js
+++ js/task-10.js
@@ -1,15 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
-
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
-
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
-
 const refs = {
   boxes: document.querySelector('#boxes'),
   createBtn: document.querySelector('[data-create]'),
@@ -21,17 +9,14 @@ refs.input.addEventListener('blur', onInputChangeAmount);
 refs.createBtn.addEventListener('click', createBoxes);
 refs.destroyBtn.addEventListener('click', destroyBoxes);
 
-let number;
-
-function onInputChangeAmount(e) {
-  number =  e.currentTarget.value;
+function onInputChangeAmount() {
+  return refs.input.value;
 }
 
-function createBoxes(amount) {
-  amount = number;
+function createBoxes() {
   let divsMarkup = '';
 
-  for (let i = 0, j = 30; i < amount; i += 1, j += 10) {
+  for (let i = 0, j = 30; i < onInputChangeAmount(); i += 1, j += 10) {
     divsMarkup += `<div class="center" style="background-color:${getRandomHexColor()};width:${j}px;height:${j}px;"></div>`
   }
 
@@ -40,4 +25,9 @@ function createBoxes(amount) {
 
 function destroyBoxes() {
   refs.boxes.innerHTML = '';
+  refs.input.value = ''; //очистка инпута посл удаления боксов
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
